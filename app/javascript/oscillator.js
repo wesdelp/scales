1c7bee12a491c080d48e05404da2840e6cf4f75a
Wes Delp <wesdelp@me.com>
2020-12-28 19:07:46 -0500

    rewrite scale playing in tone js

--- app/javascript/oscillator.js
+++ app/javascript/oscillator.js
@@ -1,51 +1,84 @@
-var context = null;
-var oscillator = null;
-function getOrCreateContext() {
-    if (!context) {
-        context = new webkitAudioContext();
-        oscillator = context.createOscillator();
-        oscillator.connect(context.destination);
-    }
-    return context;
+import * as Tone from 'tone'
 
+var synth = null
+var sequence = null
+var bpm = 60
+
+function getOrCreateSynth() {
+    if (!synth) {
+        synth = new Tone.Synth().toDestination()
+    }
+
+    return synth
 }
 
-var isStarted = false;
-function playSound(frequency, type) {
-    console.log(frequency)
-    getOrCreateContext();
-    oscillator.frequency.setTargetAtTime(frequency, context.currentTime, 0);
-    if (!isStarted) {
-        oscillator.start(0);
-        isStarted = true;
+function playScale(notes) {
+    const now = Tone.now()
+
+    if (sequence) {
+        sequence.dispose()
+    }
+
+    sequence = new Tone.Sequence(((time, note) => {
+        getOrCreateSynth().triggerAttackRelease(note, 0.1, time)
+    }), notes, "8n").start(0)
+
+    sequence.loop = false
+    Tone.Transport.bpm.value = bpm
+
+    Tone.Transport.start("+0.1")
+    Tone.Transport.stop("+1m")
+}
+
+function stopPlaying() {
+    Tone.Transport.stop()
+    Tone.Transport.cancel(0)
+}
+
+function handleButtonPressed(notes) {
+    if (Tone.Transport.state == 'started') {
+        stopPlaying()
     } else {
-        context.resume();
+        playScale(notes)
     }
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+function setUp() {
+    var scaleButton = document.getElementById('scaleButton')
 
-async function playScale(notes) {
+    scaleButton.addEventListener('click', async (e) => {
+        var notes = JSON.parse(scaleButton.dataset.notes)
 
-    for (var index = 0; index < notes.length; index++) {
-        playSound(notes[index], 'square');
-        await sleep(500);
-    }
-
-    stopSound();
-}
-
-function stopSound() {
-    context.suspend();
-}
-
-document.addEventListener("DOMContentLoaded", function(){
-    var scaleButton = document.getElementById('scaleButton');
-    scaleButton.addEventListener('click', (e) => {
-      var notes = JSON.parse(scaleButton.dataset.notes);
-      console.log(notes);
-      playScale(notes);
+        await Tone.start()
+        handleButtonPressed(notes)
     })
+}
+
+async function tearDown() {
+    stopPlaying()
+
+    if (synth) {
+        synth.dispose()
+        synth = null
+    }
+
+    if (sequence) {
+        sequence.dispose()
+        sequence = null
+    }
+}
+
+document.addEventListener('turbolinks:before-render', () => {
+    tearDown();
 });
+
+document.addEventListener('turbolinks:load', () =>
+    setUp(),
+    {
+        once: true,
+    },
+);
+
+document.addEventListener('turbolinks:render', () =>
+    setUp(),
+);
