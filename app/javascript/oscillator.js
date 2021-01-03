9525345804c608e5b65cb694f1bf2baf7e4aaa50
Wes Delp <wesdelp@me.com>
2021-01-03 15:32:36 -0500

    support playing chord progressions

--- app/javascript/oscillator.js
+++ app/javascript/oscillator.js
@@ -1,15 +1,24 @@
 import * as Tone from 'tone'
 
-var synth = null
+var monoSynth = null
+var polySynth = null
 var sequence = null
-var bpm = 60
+var bpm = 80
 
-function getOrCreateSynth() {
-    if (!synth) {
-        synth = new Tone.Synth().toDestination()
+function getMonoSynth() {
+    if (!monoSynth) {
+        monoSynth = new Tone.Synth().toDestination()
     }
 
-    return synth
+    return monoSynth
+}
+
+function getPolySynth() {
+    if (!polySynth) {
+        polySynth = new Tone.PolySynth().toDestination()
+    }
+
+    return polySynth
 }
 
 function playScale(notes) {
@@ -19,15 +28,49 @@ function playScale(notes) {
         sequence.dispose()
     }
 
+    var measures = Math.ceil(notes.length / 8)
+
     sequence = new Tone.Sequence(((time, note) => {
-        getOrCreateSynth().triggerAttackRelease(note, 0.1, time)
+        getMonoSynth().triggerAttackRelease(note, 0.1, time)
     }), notes, "8n").start(0)
+
+    startPlaying(measures)
+}
+
+function playChords(chords) {
+    const now = Tone.now()
+
+    if (sequence) {
+        sequence.dispose()
+    }
+
+    var measures = Math.ceil(chords.length / 2)
+
+    var progression = chords.map(function (chord, i) {
+        var measure = Math.floor(i / 2)
+        var beat = (i % 2) * 2
+        var time = `${measure}:${beat}`
+
+        return [time, chord]
+    })
+
+    sequence = new Tone.Part(((time, chord) => {
+        getPolySynth().triggerAttackRelease(chord, "2n", time);
+    }), progression).start(0)
+
+    startPlaying(measures)
+}
+
+function startPlaying(measures) {
+    if (!sequence) {
+        return
+    }
 
     sequence.loop = false
     Tone.Transport.bpm.value = bpm
 
     Tone.Transport.start("+0.1")
-    Tone.Transport.stop("+1m")
+    Tone.Transport.stop(`+${measures}m`)
 }
 
 function stopPlaying() {
@@ -35,7 +78,7 @@ function stopPlaying() {
     Tone.Transport.cancel(0)
 }
 
-function handleButtonPressed(notes) {
+function handlePlayScalePressed(notes) {
     if (Tone.Transport.state == 'started') {
         stopPlaying()
     } else {
@@ -43,23 +86,47 @@ function handleButtonPressed(notes) {
     }
 }
 
+function handlePlayChordsPressed(chords) {
+    if (Tone.Transport.state == 'started') {
+        stopPlaying()
+    } else {
+        playChords(chords)
+    }
+}
+
 function setUp() {
-    var scaleButton = document.getElementById('scaleButton')
+    var scaleButton = document.querySelector('.scaleButton')
 
     scaleButton.addEventListener('click', async (e) => {
         var notes = JSON.parse(scaleButton.dataset.notes)
 
         await Tone.start()
-        handleButtonPressed(notes)
+        handlePlayScalePressed(notes)
+    })
+
+    var chordsButtons = document.querySelectorAll('.chordsButton')
+
+    chordsButtons.forEach(function(chordsButton) {
+        chordsButton.addEventListener('click', async (e) => {
+            var chords = JSON.parse(chordsButton.dataset.chords)
+
+            await Tone.start()
+            handlePlayChordsPressed(chords)
+        })
     })
 }
 
 async function tearDown() {
     stopPlaying()
 
-    if (synth) {
-        synth.dispose()
-        synth = null
+    if (monoSynth) {
+        monoSynth.dispose()
+        monoSynth = null
+    }
+
+    if (polySynth) {
+        polySynth.dispose()
+        polySynth = null
     }
 
     if (sequence) {
