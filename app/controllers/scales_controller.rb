7515cc3446afda49113175f4c36702e03736221b
Wes Delp <wesdelp@me.com>
2021-01-26 22:13:02 -0500

    add link and route to generate scale

--- app/controllers/scales_controller.rb
+++ app/controllers/scales_controller.rb
@@ -3,13 +3,32 @@ class ScalesController < ApplicationController
     @notes = Scale::NOTES
     @types = Scale::TYPES
 
-    @root = params[:root] || @notes.first
-    @type = params[:type] || @types.first
+    @root = root || @notes.first
+    @type = type || @types.first
 
     @scale = ScaleBuilder.build(@root, @type)
   end
 
   def build
-    redirect_to action: :show, root: params[:root], type: params[:type]
+    redirect_to action: :show, root: root, type: type
+  end
+
+  def generate_midi
+    send_data(
+      MidiBuilder.build_progression(root, type, params[:progression].to_i),
+      filename: "#{root}#{type}_chord_progression.mid",
+      type: 'audio/midi',
+      disposition: 'attachment'
+    )
+  end
+
+  private
+
+  def root
+    params[:root]
+  end
+
+  def type
+    params[:type]
   end
 end
